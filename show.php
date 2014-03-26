a85ba981a152d07101313a839694757af21cc97c
orkunasa <orkunasa@users.noreply.github.com>
2014-03-27 00:19:23 +0200

    Create show.php

---
+++ show.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<title>140Journos | Tweet Managament</title>
+<style>
+@import url(style.css)
+</style>
+</head>
+<body>
+<?php 
+ 
+try { 
+    // Connect MongoDB server
+    $mongo = new Mongo('mongodb://url'); 
+ 
+    // Choose DB
+    $db = $mongo->selectDB('db_name'); 
+} catch(MongoConnectionException $e) { 
+    die('Baglanti Hatası : ' . $e->getMessage());  
+}
+
+// Choose Collection
+$collection = new MongoCollection($db, 'collection_name'); 
+
+// pull a cursor query  
+$cursor = $collection->find();  
+
+?>
+
+<table class="main_table">
+<tr class="title">
+<td>ID</td><td>ID_str</td><td>Created_at</td><td class="tweet">Tweet</td><td class="addr">ADDR</td><td class="update">Update</td>
+</tr>
+<?php foreach($cursor as $document) { ?>
+<tr><td><?php echo $document['_id']; ?></td>
+<td><?php print_r($document['id_str']);  ?></td><td><?php print_r($document['created_at']);  ?></td><td class="tweet"><?php print_r($document['tweet']);  ?></td><td class="addr"><?php print_r($document['addr']);  ?></td><td class="update"><a href="update.php?id=<?php echo $document['_id']; ?>">Edit</a></td>
+</tr>
+<?php } ?>
+</table>
+  
+?>
+</body>
+</html>
