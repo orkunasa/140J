027df57c3bf78c3435861d364674ab21c5d10c2f
orkunasa <orkunasa@users.noreply.github.com>
2014-03-28 14:58:56 +0200

    Create update_str_save.php

---
+++ update_str_save.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<title>140Journos | Verileri Listele</title>
+<style>
+@import url(style.css)
+</style>
+</head>
+<body>
+<?php
+
+try { 
+    // Connect MongoDB server
+    $mongo = new Mongo('URL'); 
+ 
+    // Choose DB
+    $db = $mongo->selectDB('db_name'); 
+} catch(MongoConnectionException $e) { 
+    die('Baglanti Hatası : ' . $e->getMessage());  
+}
+
+// Choose Collection
+$collection = new MongoCollection($db, 'logtest1'); 
+
+
+
+if(!empty($_POST)){
+ 
+    $id = $_POST['id'];
+    $tweet = $_POST['tweet'];
+
+    $query = array('_id'=>new MongoId($id));
+    $update = $collection->findOne($query);
+
+    $update['tweet'] = $tweet;
+    $collection->save($update);
+ 
+    echo 'Güncelleme İşlemi Başarılı!';
+echo "<br>", $id;
+
+}
+ ?>
+
+<br>
+<a href="update_str.php?id=<?php echo $id ?>">Geri</a>
